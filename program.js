d389aee7a94dfa752012bde2503c180a61d48e4a
NiLe1204 <nihad_1204@hotmail.com>
2022-12-03 13:04:59 +0100

    ucenje

--- program.js
+++ program.js
@@ -1,7 +1,37 @@
 console.log("Hello World");
 console.log("Drugi Hello da se ne patimo");
 
+var number1 = 23;
+var number2 = 34;
+var rezultat = number1+number2;
+console.log(rezultat);
+
+
 let temperature = 33;
-temperature = "Danas je bio divan i sunčan dan";
-console.log (temperature);
+temperature = "toplo je";
+console.log(temperature);
+
+let number = 011;
+console.log (number);
+
+const piConstant = 3.14;
+console.log(piConstant);
+
+
+let number1 = 1;
+let number2 = 34;
+let sum = sum(number1 + number2);
+
+let rezultat = sum(23, 34);
+
+function sum(num1, num2);{
+    var rez = num1 + num2;
+    return num1 + num2;
+}
+console.log(num1);
+
+
+
+
+
 
